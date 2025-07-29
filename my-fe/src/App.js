5cfc47c5abf9781e0cc53fa61b58eec8aeabc32c
QUANG-SanBay <quangsanbay@gmail.com>
2025-07-29 13:31:08 +0700

    learn more 'react-router-dom'

--- my-fe/src/App.js
+++ my-fe/src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
-import Button from './components/Button';
+import {Link, Route, Routes} from 'react-router-dom'
+import Contact from './pages/Contact'
+import New from './pages/New'
+import Home from './pages/Home'
 function App() {
   return (
     <>
-      <Button>click me!!</Button>
-      <Button primary={true}>primary btn</Button>
-      <Button danger={true}>danger btn</Button>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/New'>New</Link>
+          </li>
+          <li>
+            <Link to='/Contact'>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/New' element={<New/>}></Route>
+        <Route path='/Contact' element={<Contact/>}></Route>
+      </Routes>
     </>
   )
 }
